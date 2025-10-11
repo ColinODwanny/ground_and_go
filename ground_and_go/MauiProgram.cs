6fcb724494a9035179a639e5524f1327e2428456
Sam <sreynebeau11@gmail.com>
2025-10-10 23:23:48 -0500

    added to appshell.xaml for tabs, added icon package, and reorganized page files into pages folder

--- ground_and_go/MauiProgram.cs
+++ ground_and_go/MauiProgram.cs
@@ -1,24 +1,27 @@
-﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging;
+using MauiIcons.Material;
 
 namespace ground_and_go;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().UseMaterialMauiIcons();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
---
+++ ground_and_go/Pages/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace ground_and_go.Pages;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		
+	}
+}
