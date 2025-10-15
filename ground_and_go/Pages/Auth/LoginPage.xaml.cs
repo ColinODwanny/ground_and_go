1569661d6a14536fea713a307f787c9aaac4ae61
Devlin Delegard <Delegardde86@uwosh.edu>
2025-10-14 19:18:40 -0500

    Login xaml Implemented
    The login UI is implemented, excluding any logo we may decide upon later.
    Navigation is not yet implemented, but that can be done so at a later date.

--- ground_and_go/Pages/Auth/LoginPage.xaml.cs
+++ ground_and_go/Pages/Auth/LoginPage.xaml.cs
@@ -2,9 +2,23 @@
 namespace ground_and_go.Pages.Auth;
 
 public partial class LoginPage : ContentPage 
-{ 
-    public LoginPage() 
-    { 
-        InitializeComponent(); 
+{
+    public LoginPage()
+    {
+        InitializeComponent();
     } 
+    
+    private async void LoginClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            if (sender is not Button button) return;
+
+            //var username = button.
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"Error: {ex.Message}", "OK");
+        }
+    }
 }
