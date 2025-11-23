75ee60e91581823387e01b2f8f5933d5b4f16086
Colin O’Dwanny <colinodwanny@MacBook-Air-32.local>
2025-11-23 16:31:26 -0600

    adding mindfulness functionality

--- ground_and_go/App.xaml.cs
+++ ground_and_go/App.xaml.cs
@@ -1,14 +1,32 @@
-﻿namespace ground_and_go;
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ground_and_go;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
-	}
+    public App()
+    {
+        InitializeComponent();
+    }
 
-	protected override Window CreateWindow(IActivationState? activationState)
-	{
-		return new Window(new AppShell());
-	}
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+
+    protected override async void OnStart()
+    {
+        var db = IPlatformApplication.Current.Services.GetService<Database>();
+
+        if (db is null)
+        {
+            Console.WriteLine("Database service not found.");
+            return;
+        }
+
+        await db.EnsureInitializedAsync();
+        await db.LoadExercises();
+        await db.LoadMindfulness();
+    }
 }
